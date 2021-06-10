a7202ace5403048a01916659dd58cc0433079241
yafuyafu <yasaki418@gmail.com>
2021-06-10 17:33:24 +0900

    asaki 3rd

--- lib/day8_crawler.ex
+++ lib/day8_crawler.ex
@@ -4,13 +4,30 @@ defmodule Day8Crawler do
     ["https://thewaggletraining.github.io/"]
     |> SimpleCrawler2.main()
     |> insertData()
+    |> IO.inspect()
+    |> (&List.flatten(["uri", "domain", "body"], &1)).()
+    |> IO.inspect()
+    |> CSV.encode()
+    |> Enum.take(5)
+    |> Enum.join()
+    |> (&File.write("page_info.csv",&1)).()
+    #|> IO.inspect()
   end
 
   def insertData(data) do
     page_info = %Day8Crawler.PageInfo{}
-    Enum.each(data, fn x ->
+    Enum.map(data, fn x ->
       Day8Crawler.PageInfo.changeset(page_info, x)
       |> Day8Crawler.Repo.insert()
+      |> dataToList()
+
+      # |> IO.inspect()
     end)
+  end
+
+  def dataToList(data) do
+    # struct = elem(data, 1)
+    {:ok, struct} = data
+    [struct.uri, struct.domain, struct.body]
   end
 end
